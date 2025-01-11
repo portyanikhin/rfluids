64dfa07037e49a9d8568ea648c17f4a5223147e9
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2025-01-11 18:05:25 +0300

    Added `Substance` and `SubstanceCategory` enums

--- fluids-rs/src/enums/mod.rs
+++ fluids-rs/src/enums/mod.rs
@@ -4,8 +4,10 @@ pub use humid_air_parameters::*;
 pub use input_pairs::*;
 pub use parameters::*;
 pub use phases::*;
+pub use substances::*;
 
 mod humid_air_parameters;
 mod input_pairs;
 mod parameters;
 mod phases;
+mod substances;
