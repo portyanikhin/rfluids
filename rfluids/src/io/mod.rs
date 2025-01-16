ac339dec65a6d372a7cd584c70085902c4a8f761
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2025-01-16 23:55:03 +0300

    Added `fluid_input` module

--- rfluids/src/io/mod.rs
+++ rfluids/src/io/mod.rs
@@ -1,11 +1,22 @@
 //! CoolProp inputs/outputs.
 
+pub use fluid_input::*;
 pub use fluid_input_pair::*;
 pub use fluid_param::*;
 pub use humid_air_param::*;
 pub use phase::*;
 
+mod fluid_input;
 mod fluid_input_pair;
 mod fluid_param;
 mod humid_air_param;
 mod phase;
+
+/// CoolProp keyed input.
+pub trait KeyedInput<K> {
+    /// Specified key.
+    fn key(&self) -> K;
+
+    /// Specified value _(in SI units)_.
+    fn si_value(&self) -> f64;
+}
