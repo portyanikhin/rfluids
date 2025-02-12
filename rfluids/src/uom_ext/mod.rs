a0dc07f502d8e6bc8b4d4efcc5b051de842bec24
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2025-02-12 19:22:13 +0300

    Add `uom_ext::pressure_coefficient` module

--- rfluids/src/uom_ext/mod.rs
+++ rfluids/src/uom_ext/mod.rs
@@ -1,3 +1,5 @@
 //! Additional type aliases for combined quantities
 //! (e.g., `uom::si::Quantity<uom::si::ISQ<...>, uom::si::SI<f64>, f64>`)
 //! which are not included in the [`uom`](https://crates.io/crates/uom) crate.
+
+pub mod pressure_coefficient;
