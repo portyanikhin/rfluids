24745befe7f1ea20e4c12a3361e52c21dd4f3cbd
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2025-02-13 15:56:58 +0300

    Update prelude imports

--- rfluids/src/prelude.rs
+++ rfluids/src/prelude.rs
@@ -7,13 +7,16 @@ pub mod fluid {
     //! Prelude imports of [`fluid`](crate::fluid) module.
 
     pub use crate::fluid::*;
-    pub use crate::io::{FluidInput, Phase};
+    pub use crate::io::{fluid_input, FluidInput, Phase};
     pub use crate::state_variant::*;
     pub use crate::substance::*;
     pub use crate::uom::si::f64::{
-        AvailableEnergy, MassDensity, MolarConcentration, MolarEnergy, MolarHeatCapacity, Pressure,
-        Ratio, SpecificHeatCapacity, ThermodynamicTemperature,
+        AvailableEnergy, DynamicViscosity, Force, Length, MassDensity, MolarConcentration,
+        MolarEnergy, MolarHeatCapacity, Pressure, Ratio, SpecificHeatCapacity,
+        TemperatureCoefficient, ThermalConductivity, ThermodynamicTemperature, Velocity,
     };
+    pub use crate::uom_ext::pressure_coefficient::PressureCoefficient;
+    pub use crate::uom_ext::surface_tension::SurfaceTension;
 }
 
 pub mod native {
