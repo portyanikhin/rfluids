2124b22a1b3b8696f503c2212186238a0ef2cbae
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2025-02-14 17:29:18 +0300

    Refactoring

--- rfluids/src/ops.rs
+++ rfluids/src/ops.rs
@@ -1,15 +1,15 @@
 use std::ops::{Div, Mul};
 
-pub(crate) fn mul<R, L, O, E>(lhs: Result<R, E>, rhs: Result<L, E>) -> Result<O, E>
+pub(crate) fn mul<L, R, O, E>(lhs: Result<L, E>, rhs: Result<R, E>) -> Result<O, E>
 where
-    R: Mul<L, Output = O>,
+    L: Mul<R, Output = O>,
 {
     lhs.and_then(|l| rhs.map(|r| l * r))
 }
 
-pub(crate) fn div<R, L, O, E>(lhs: Result<R, E>, rhs: Result<L, E>) -> Result<O, E>
+pub(crate) fn div<L, R, O, E>(lhs: Result<L, E>, rhs: Result<R, E>) -> Result<O, E>
 where
-    R: Div<L, Output = O>,
+    L: Div<R, Output = O>,
 {
     lhs.and_then(|l| rhs.map(|r| l / r))
 }
