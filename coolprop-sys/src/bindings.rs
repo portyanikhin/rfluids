93fceeaa6000f3bead75e65efcaaf1885fd04f8f
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2025-02-28 18:12:06 +0300

    Update lints

--- coolprop-sys/src/bindings.rs
+++ coolprop-sys/src/bindings.rs
@@ -2,9 +2,13 @@
 
 #![allow(
     dead_code,
+    missing_docs,
     non_camel_case_types,
     non_snake_case,
-    non_upper_case_globals
+    non_upper_case_globals,
+    unsafe_op_in_unsafe_fn,
+    clippy::all,
+    clippy::pedantic
 )]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
