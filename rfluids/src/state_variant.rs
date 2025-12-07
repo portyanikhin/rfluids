950f2ea106763b255b9f5581cc0c6267d43d243e
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2025-12-07 21:54:45 +0300

    style: update formatting

--- rfluids/src/state_variant.rs
+++ rfluids/src/state_variant.rs
@@ -7,12 +7,10 @@ impl StateVariant for Defined {}
 
 impl StateVariant for Undefined {}
 
-/// A marker that determines the _presence_ of
-/// a defined thermodynamic state.
+/// A marker that determines the _presence_ of a defined thermodynamic state.
 #[derive(Debug)]
 pub struct Defined;
 
-/// A marker that determines the _absence_ of
-/// a defined thermodynamic state.
+/// A marker that determines the _absence_ of a defined thermodynamic state.
 #[derive(Debug)]
 pub struct Undefined;
