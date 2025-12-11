fd27119d414a2e8f3d451cee14b47be7cb414d53
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2025-12-11 19:14:38 +0300

    feat: add `GlobalParam` to prelude re-exports

--- rfluids/src/prelude.rs
+++ rfluids/src/prelude.rs
@@ -3,7 +3,10 @@
 pub use crate::{
     fluid::{Fluid, backend::*},
     humid_air::HumidAir,
-    io::{FluidInput, FluidInputPair, FluidParam, FluidTrivialParam, HumidAirInput, Input, Phase},
+    io::{
+        FluidInput, FluidInputPair, FluidParam, FluidTrivialParam, GlobalParam, HumidAirInput,
+        Input, Phase,
+    },
     native::{AbstractState, CoolProp},
     state_variant::*,
     substance::{BinaryMix, BinaryMixKind, CustomMix, IncompPure, PredefinedMix, Pure, Substance},
