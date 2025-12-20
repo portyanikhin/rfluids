95bcf7ad64e0736606086bc4b2fd2c1481bb6eb7
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2025-12-20 17:43:03 +0300

    feat: implement configuration management with `Config` struct, `read` and `update` functions

---
+++ rfluids/src/config/declare.rs
@@ -0,0 +1,44 @@
+macro_rules! declare_config {
+    ($($(#[$meta:meta])* $field:ident : $ty:ty $(= $default:expr)? => $key:ident),* $(,)?) => {
+        /// Library configuration state.
+        ///
+        /// This configuration mirrors the configuration options available in `CoolProp`.
+        /// Each field corresponds directly to a `CoolProp` configuration parameter and
+        /// is synchronized with the underlying `CoolProp` library when
+        /// [`config::update`](crate::config::update) is called.
+        ///
+        /// # Thread Safety
+        ///
+        /// The configuration is stored in a global state protected by an
+        /// [`RwLock`](std::sync::RwLock). Use [`config::read`](crate::config::read) to get
+        /// the current configuration and [`config::update`](crate::config::update) to modify it.
+        #[derive(Clone, Debug, PartialEq, bon::Builder)]
+        #[non_exhaustive]
+        pub struct Config {
+            $(
+                $(#[$meta])*
+                $(#[builder(default = $default)])?
+                pub $field: $ty,
+            )*
+        }
+
+        impl Config {
+            fn update(&mut self, new: Self) -> Result<(), crate::native::CoolPropError> {
+                $(if self.$field != new.$field {
+                    let key = crate::io::ConfigKey::$key;
+                    crate::native::CoolProp::set_config(key, &new.$field)?;
+                    self.$field = new.$field;
+                })*
+                Ok(())
+            }
+        }
+
+        impl Default for Config {
+            fn default() -> Self {
+                Config::builder().build()
+            }
+        }
+    };
+}
+
+pub(super) use declare_config;
--- rfluids/src/config/mod.rs
+++ rfluids/src/config/mod.rs
@@ -1,1 +1,263 @@
 //! Library configuration.
+
+#![allow(clippy::struct_excessive_bools)]
+
+mod declare;
+
+use std::{
+    path::PathBuf,
+    sync::{LazyLock, RwLock},
+};
+
+use declare::declare_config;
+
+use crate::native::CoolPropError;
+
+static CONFIG: LazyLock<RwLock<Config>> = LazyLock::new(|| RwLock::new(Config::default()));
+
+declare_config! {
+    /// If `true`, evaluation of the stability of critical point will be skipped and point will be
+    /// assumed to be stable.
+    ///
+    /// **Default:** `false`
+    assume_critical_point_is_stable: bool = false => AssumeCriticalPointIsStable,
+
+    /// if `true`, any temperature within `1 uK` of the critical temperature will be considered to
+    /// be **AT** the critical point.
+    ///
+    /// **Default:** `true`
+    critical_within_1uk: bool = true => CriticalWithin1Uk,
+
+    /// If `true`, when possible, `CoolProp` will skip checking whether values are inside the
+    /// property limits.
+    ///
+    /// **Default:** `false`
+    dont_check_prop_limits: bool = false => DontCheckPropLimits,
+
+    /// If `true`, the critical splines will be used in the near-vicinity of the critical point.
+    ///
+    /// **Default:** `true`
+    enable_critical_splines: bool = true => EnableCriticalSplines,
+
+    /// if `true`, the superancillary functions will be used for VLE of pure fluids.
+    ///
+    /// **Default:** `true`
+    enable_superancillaries: bool = true => EnableSuperancillaries,
+
+    /// If `true`, when doing water-based mixture dewpoint calculations, use Henry’s Law to
+    /// generate guesses for liquid-phase composition.
+    ///
+    /// **Default:** `false`
+    henrys_law_to_generate_vle_guesses: bool = false => HenrysLawToGenerateVleGuesses,
+
+    /// If `true`, for mixtures, the molar gas constant _(R)_ will be set to the CODATA value.
+    ///
+    /// **Default:** `true`
+    normalize_gas_constants: bool = true => NormalizeGasConstants,
+
+    /// If `true`, and a pair of binary interaction pairs to be added is already there, rather than
+    /// not adding the binary interaction pair _(and probably throwing an exception)_, overwrite
+    /// it.
+    ///
+    /// **Default:** `false`
+    overwrite_binary_interaction: bool = false => OverwriteBinaryInteraction,
+
+    /// if `true`, and a departure function to be added is already there, rather than not adding
+    /// the departure function _(and probably throwing an exception)_, overwrite it.
+    ///
+    /// **Default:** `false`
+    overwrite_departure_fn: bool = false => OverwriteDepartureFn,
+
+    /// If `true`, and a substance is added to the substances library that is already there, rather
+    /// than not adding the substance _(and probably throwing an exception)_, overwrite it.
+    ///
+    /// **Default:** `false`
+    overwrite_substances: bool = false => OverwriteSubstances,
+
+    /// Starting pressure in `Pa` for phase envelope construction.
+    ///
+    /// **Default:** `100.0`
+    phase_envelope_start_pressure_pa: f64 = 100.0 => PhaseEnvelopeStartPressurePa,
+
+    /// The value for the ideal gas constant in `J/mol/K` according to CODATA 2022. This value is
+    /// used to harmonize all the ideal gas constants. This is especially important in the critical
+    /// region.
+    ///
+    /// **Default:** `8.314_462_618_153_24`
+    ru_codata: f64 = 8.314_462_618_153_24 => RUCodata,
+
+    /// The minimal delta to be used in tracing out the spinodal; make sure that the EOS has a
+    /// spinodal at this value of `delta=rho/rho_r`.
+    ///
+    /// **Default:** `0.5`
+    spinodal_min_delta: f64 = 0.5 => SpinodalMinDelta,
+
+    /// If `true`, calls to the vectorized versions of `PropsSI` use the previous state as guess
+    /// value while looping over the input vectors, only makes sense when working with a single
+    /// fluid and with points that are not too far from each other.
+    ///
+    /// **Default:** `false`
+    use_guesses_in_props_si: bool = false => UseGuessesInPropsSi,
+
+    /// An alternative path to be provided to the directory that contains `REFPROP`’s fluids and
+    /// mixtures directories. If provided, the `SETPATH` function will be called with this
+    /// directory prior to calling any `REFPROP` functions.
+    ///
+    /// **Default:** `None`
+    alt_refprop_path: Option<PathBuf> => AltRefpropPath,
+
+    /// An alternative path to the shared library file. If provided, it will be used to load
+    /// `REFPROP`.
+    ///
+    /// **Default:** `None`
+    alt_refprop_lib_path: Option<PathBuf> => AltRefpropLibPath,
+
+    /// An alternative path to the `HMX.BNC` file. If provided, it will be passed into `REFPROP`’s
+    /// `SETUP` or `SETMIX` routines.
+    ///
+    /// **Default:** `None`
+    alt_refprop_hmx_bnc_path: Option<PathBuf> => AltRefpropHmxBncPath,
+
+    /// If `true`, if the binary interaction parameters in `REFPROP` are estimated, throw an error
+    /// rather than silently continuing.
+    ///
+    /// **Default:** `false`
+    refprop_dont_estimate_interaction_params: bool = false => RefpropDontEstimateInteractionParams,
+
+    /// If `true`, if the binary interaction parameters in `REFPROP` are unable to be estimated,
+    /// silently continue rather than failing.
+    ///
+    /// **Default:** `false`
+    refprop_ignore_error_estimated_interaction_params: bool = false =>
+        RefpropIgnoreErrorEstimatedInteractionParams,
+
+    /// If `true`, rather than using the highly-accurate pure fluid equations of state, use the
+    /// pure-fluid EOS from `GERG-2008`.
+    ///
+    /// **Default:** `false`
+    refprop_use_gerg: bool = false => RefpropUseGerg,
+
+    /// If `true`, rather than using the highly-accurate pure fluid equations of state, use the
+    /// Peng-Robinson EOS.
+    ///
+    /// **Default:** `false`
+    refprop_use_peng_robinson: bool = false => RefpropUsePengRobinson,
+
+    /// If provided, this path will be the root directory for the tabular data. Otherwise,
+    /// `${HOME}/.CoolProp/Tables` is used.
+    ///
+    /// **Default:** `None`
+    alt_tables_path: Option<PathBuf> => AltTablesPath,
+
+    /// The first character of this string will be used as the separator between the number
+    /// fraction.
+    ///
+    /// **Default:** `'.'`
+    float_punctuation: char = '.' => FloatPunctuation,
+
+    /// The delimiter to be used when converting a list of strings to a string.
+    ///
+    /// **Default:** `','`
+    list_punctuation: char = ',' => ListPunctuation,
+
+    /// The maximum allowed size of the directory that is used to store tabular data.
+    ///
+    /// **Default:** `1.0`
+    max_table_dir_size_in_gb: f64 = 1.0 => MaxTableDirSizeInGb,
+
+    /// If `true`, the raw, uncompressed tables will also be written to file.
+    ///
+    /// **Default:** `false`
+    save_raw_tables: bool = false => SaveRawTables,
+
+    /// If `true`, the library will always be reloaded, no matter what is currently loaded.
+    ///
+    /// **Default:** `false`
+    vtpr_always_reload_lib: bool = false => VtPrAlwaysReloadLib,
+
+    /// The path to the directory containing the UNIFAC JSON files.
+    ///
+    /// **Default:** `None`
+    vtpr_unifac_path: Option<PathBuf> => VtPrUnifacPath,
+}
+
+/// Error during [`config::update`](update).
+#[derive(Clone, Debug, Eq, PartialEq, thiserror::Error)]
+#[error("Unable to update configuration! {0}")]
+pub struct ConfigError(#[from] CoolPropError);
+
+/// Returns a clone of the current library configuration.
+///
+/// This function acquires a read lock on the global configuration state and returns
+/// a clone of the current [`Config`].
+///
+/// # Panics
+///
+/// Panics if the internal read lock is poisoned. This should only occur if another thread
+/// panicked while holding the write lock.
+///
+/// # Examples
+///
+/// ```no_run
+/// use rfluids::config;
+///
+/// let current_config = config::read();
+/// println!("{current_config:?}");
+/// ```
+pub fn read() -> Config {
+    CONFIG.read().unwrap().clone()
+}
+
+/// Updates the library configuration with new values.
+///
+/// To create a new configuration, use [`Config::builder`] to build a configuration
+/// with only the fields you want to change.
+///
+/// This function compares each field of the new configuration with the current one.
+/// For any field that has changed, it synchronizes the value with the underlying `CoolProp`
+/// library before updating the global state.
+///
+/// # Errors
+///
+/// Returns a [`ConfigError`] if `CoolProp` fails to accept any of the new configuration values.
+/// In case of error, the configuration state remains unchanged.
+///
+/// # Panics
+///
+/// Panics if the internal write lock is poisoned. This should only occur if another thread
+/// panicked while holding the write lock.
+///
+/// # Examples
+///
+/// ```no_run
+/// use rfluids::config::{self, Config};
+///
+/// // Update specific configuration options
+/// let new_config = Config::builder()
+///     .enable_critical_splines(false)
+///     .enable_superancillaries(false)
+///     .dont_check_prop_limits(true)
+///     .build();
+///
+/// config::update(new_config)?;
+/// # Ok::<(), rfluids::Error>(())
+/// ```
+pub fn update(new: Config) -> Result<(), ConfigError> {
+    let mut config = CONFIG.write().unwrap();
+    config.update(new)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn read_default() {
+        // When
+        let res = read();
+
+        // Then
+        assert_eq!(res, Config::default());
+    }
+}
